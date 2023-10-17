e7165a3cbc604d51dfe1c5a26c4cc81e5c0dfed4
Gabriel Melo <melogoncalvesbiel@gmail.com>
2023-10-16 22:57:15 -0300

    feat: update task

--- src/main/java/tech/gabrielmelo/todolist/controller/TaskController.java
+++ src/main/java/tech/gabrielmelo/todolist/controller/TaskController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 import tech.gabrielmelo.todolist.models.TaskModel;
 import tech.gabrielmelo.todolist.repository.ITaskRepository;
+import tech.gabrielmelo.todolist.utils.Utils;
 
 @RestController
 @RequestMapping("/tasks")
@@ -54,8 +55,11 @@ public class TaskController {
   @PutMapping("/{id}")
   public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
     var idUser = request.getAttribute("idUser");
-    taskModel.setIdUser((UUID) idUser);
-    taskModel.setId(id);
-    return this.taskRepository.save(taskModel);
+
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }
